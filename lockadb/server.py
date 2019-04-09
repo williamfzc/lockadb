4c0f479ca6c20119598cc954c147cd9c7cdf5aa4
William Feng <178894043@qq.com>
2019-04-09 10:57:38 +0800

    add: device management

--- lockadb/server.py
+++ lockadb/server.py
@@ -1,17 +1,49 @@
 from fastapi import FastAPI
 from whenconnect import when_connect, when_disconnect
+from loguru import logger
+
+
+class Device(object):
+    class DeviceStatus(object):
+        FREE = 0
+        BUSY = 1
+
+    def __init__(self, device_id):
+        self.device_id = device_id
+        self.status = self.DeviceStatus.FREE
+
+    def is_free(self):
+        return self.status == self.DeviceStatus.FREE
+
+    def acquire(self):
+        self.status = self.DeviceStatus.BUSY
+
+    def release(self):
+        self.status = self.DeviceStatus.FREE
+
+    def __hash__(self):
+        return hash(self.device_id)
 
 
 class DeviceManager(object):
-    DEVICE_SET = set()
+    DEVICE_DICT = dict()
 
     @classmethod
-    def add(cls, device):
-        cls.DEVICE_SET.add(device)
+    def add(cls, device_id: str) -> bool:
+        if device_id in cls.DEVICE_DICT:
+            logger.warning('device {} already existed'.format(device_id))
+            return False
+        device = Device(device_id)
+        cls.DEVICE_DICT[device_id] = device
+        return True
 
     @classmethod
-    def remove(cls, device):
-        cls.DEVICE_SET.remove(device)
+    def remove(cls, device_id: str) -> bool:
+        if device_id not in cls.DEVICE_DICT:
+            logger.warning('device {} not existed'.format(device_id))
+            return False
+        del cls.DEVICE_DICT[device_id]
+        return True
 
 
 when_connect(device='any', do=DeviceManager.add)
@@ -22,12 +54,13 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"Hello": "World"}
+    return {"hello": "from lockadb server"}
 
 
 @app.get("/devices")
 def all_devices():
-    return {"devices": list(DeviceManager.DEVICE_SET)}
+    device_id_list = [i.device_id for i in DeviceManager.DEVICE_DICT.values()]
+    return {"devices": device_id_list}
 
 
 # start server with:
