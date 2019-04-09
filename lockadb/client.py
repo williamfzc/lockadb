d3dd295a6b8f3f07107546010fc65802926f5c3b
William Feng <178894043@qq.com>
2019-04-09 18:09:35 +0800

    update: client functions

--- lockadb/client.py
+++ lockadb/client.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from lockadb.config import SERVER_URL_PREFIX
 
 
@@ -8,9 +9,27 @@ def heartbeat() -> bool:
 
 def get_devices() -> str:
     request_url = SERVER_URL_PREFIX + '/devices'
-    return requests.get(request_url).text
+    return json.loads(requests.get(request_url).text)
+
+
+def acquire_device(device_id: str):
+    request_url = SERVER_URL_PREFIX + '/devices/acquire?device_id=' + device_id
+    return json.loads(requests.post(request_url).text)
+
+
+def release_device(device_id: str):
+    request_url = SERVER_URL_PREFIX + '/devices/release?device_id=' + device_id
+    return json.loads(requests.post(request_url).text)
 
 
 if __name__ == '__main__':
+    TEST_DEVICE_ID = '123456F'
+
     devices = get_devices()
     print(devices)
+
+    result = acquire_device(TEST_DEVICE_ID)
+    print(result)
+
+    result = release_device(TEST_DEVICE_ID)
+    print(result)
